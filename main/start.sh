25534fa9f1b673d71bd81cf86cd0436b91e8f6c7
BINAH25 <visteen192@gmail.com>
2025-05-16 15:23:31 +0000

    jenkins url added

--- main/start.sh
+++ main/start.sh
@@ -17,5 +17,5 @@ flask db upgrade || echo "Upgrade failed. Possible mismatch in migration history
 
 # Start the app
 echo "Starting app..."
-python main.py
+exec gunicorn -b 0.0.0.0:5000 main:app
 
