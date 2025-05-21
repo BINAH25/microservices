bfe848b9ad3a9471d29c8ed0308612cae0b73280
BINAH25 <visteen192@gmail.com>
2025-05-21 07:58:39 +0000

    error fixed

--- main/start.sh
+++ main/start.sh
@@ -6,7 +6,7 @@ if [ ! -d "migrations/versions" ]; then
     echo "Initializing migrations..."
     flask db init
 fi
-flask db revision --rev-id=a4bde34a1ddf
+flask db revision --rev-id=eef4f8759088
 
 # Always try to make migrations (safe if no changes)
 echo "Running migrations..."
